d0a456a44e1e0d2f043220fb1355a2a1c56c8361
bahcor2 <bruno.ahcor2@gmail.com>
2024-11-09 15:36:30 -0300

    commit projetos

---
+++ movement-service/src/test/java/com/bahcor/movement_service/MovementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bahcor.movement_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ rules-service/src/test/java/com/bahcor/rules_service/RulesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bahcor.rules_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RulesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
